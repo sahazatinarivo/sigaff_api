6cefd690ba2e234a1a868239596d00c83e0e4ac6
sahaza_bureau <sahazatinarivo@gmail.com>
2025-03-13 14:51:25 +0300

    modification

--- application/config/routes.php
+++ application/config/routes.php
@@ -11,3 +11,9 @@ $config['uri_protocol'] = 'REQUEST_URI';
 
 $route['verification-im.html']      = "ApiController/verificationIm";
 $route['verification-cin.html']     = "ApiController/verificationCin";
+$route['get-fonction.html']     = "ApiController/getFonction";
+$route['get-dren.html']     = "ApiController/getDren";
+$route['get-cisco.html']     = "ApiController/getCisco";
+$route['get-etab.html']     = "ApiController/getEtab";
+$route['get-dir.html']     = "ApiController/getDir";
+$route['get-service.html']     = "ApiController/getServices";
--- application/controllers/ApiController.php
+++ application/controllers/ApiController.php
@@ -31,7 +31,7 @@ class ApiController extends ApiConfig {
             $this->output
                 ->set_content_type('application/json')
                 ->set_output(json_encode([ "_status"=> $_status, "imRes"=>$ims ]));
-        };
+        }
     }
 
     public function verificationCin(){
@@ -57,7 +57,149 @@ class ApiController extends ApiConfig {
             $this->output
                 ->set_content_type('application/json')
                 ->set_output(json_encode([ "_status"=> $_status, "imRes"=>$ims,"data"=>$__res ]));
-        };
+        }
     }
 
+    public function getFonction(){
+        $output = array();
+
+        $typeLoc = isset($_GET['typeLoc']) ? $_GET['typeLoc'] : 0;
+
+        if ($this->verify_api_key()) {
+            $_status = 'ko';
+
+            $__res = $this->db->from('t_fonction')
+                             ->where('id_type_loc',$typeLoc)
+                             ->get()
+                             ->result();
+
+            if (count($__res) > 0) {
+                $_status = 'ok';
+            }
+
+            $this->output
+                ->set_content_type('application/json')
+                ->set_output(json_encode([ "_status"=> $_status,"data"=>$__res ]));
+        }
+    }
+
+    public function getDren(){
+        $output = array();
+
+        if ($this->verify_api_key()) {
+            $_status = 'ko';
+
+            $__res = $this->db->from('t_dren')
+                             ->get()
+                             ->result();
+
+            if (count($__res) > 0) {
+                $_status = 'ok';
+            }
+
+            $this->output
+                ->set_content_type('application/json')
+                ->set_output(json_encode([ "_status"=> $_status,"data"=>$__res ]));
+        }
+    }
+
+    public function getCisco(){
+        $output = array();
+
+        $codeDren = isset($_GET['codeDren']) ? $_GET['codeDren'] : 0;
+
+        if ($this->verify_api_key()) {
+            $_status = 'ko';
+
+            $__res = $this->db->from('t_cisco')
+                             ->where('code_dren',$codeDren)
+                             ->get()
+                             ->result();
+
+            if (count($__res) > 0) {
+                $_status = 'ok';
+            }
+
+            $this->output
+                ->set_content_type('application/json')
+                ->set_output(json_encode([ "_status"=> $_status,"data"=>$__res ]));
+        }
+    }
+
+    public function getEtab(){
+        $output = array();
+
+        $codeDren = isset($_GET['codeDren']) ? $_GET['codeDren'] : 0;
+        $codeCisco = isset($_GET['codeCisco']) ? $_GET['codeCisco'] : 0;
+        $codeFonction = isset($_GET['codeFonction']) ? $_GET['codeFonction'] : null;
+
+        if ($this->verify_api_key()) {
+            $_status = 'ko';
+
+            $this->db->from('t_etab');
+            $this->db->where('code_dren',$codeDren);
+            $this->db->where('code_cisco',$codeCisco);
+            if ($codeFonction <> null ) {
+                if ($codeFonction > 0) {
+                    $this->db->where('code_niveau',$codeFonction);
+                }else{
+                    $this->db->where('existe_presco',1);
+                } 
+            }
+
+            $__res = $this->db->get()->result();
+
+            if (count($__res) > 0) {
+                $_status = 'ok';
+            }
+
+            $this->output
+                ->set_content_type('application/json')
+                ->set_output(json_encode([ "_status"=> $_status,"data"=>$__res ]));
+        }
+    }
+
+    public function getDir(){
+        $output = array();
+        $codeDren = isset($_GET['codeDi']) ? $_GET['codeDren'] : 0;
+
+        if ($this->verify_api_key()) {
+            $_status = 'ko';
+
+            $__res = $this->db->from('t_direction')
+                             ->get()
+                             ->result();
+
+            if (count($__res) > 0) {
+                $_status = 'ok';
+            }
+
+            $this->output
+                ->set_content_type('application/json')
+                ->set_output(json_encode([ "_status"=> $_status,"data"=>$__res ]));
+        }
+    }
+
+
+    public function getServices(){
+        $output = array();
+        $codeDir = isset($_GET['codeDir']) ? $_GET['codeDir'] : 0;
+
+        if ($this->verify_api_key()) {
+            $_status = 'ko';
+
+            $__res = $this->db->from('t_service')
+                             ->where('code_direction',$codeDir)
+                             ->get()
+                             ->result();
+
+            if (count($__res) > 0) {
+                $_status = 'ok';
+            }
+
+            $this->output
+                ->set_content_type('application/json')
+                ->set_output(json_encode([ "_status"=> $_status,"data"=>$__res ]));
+        }
+    }
 }
